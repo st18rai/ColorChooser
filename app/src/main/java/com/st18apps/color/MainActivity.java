c54a8b1be6ec4059a79b939422fb6fc7255f12b0
st18rai <st18rai@gmail.com>
2017-09-14 12:30:41 +0300

    Added ability to set color manually.

--- app/src/main/java/com/st18apps/color/MainActivity.java
+++ app/src/main/java/com/st18apps/color/MainActivity.java
@@ -1,60 +1,102 @@
 package com.st18apps.color;
 
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
-    private SeekBar red;
-    private SeekBar green;
-    private SeekBar blue;
-    private LinearLayout palette;
+    private SeekBar sbRed;
+    private SeekBar sbGreen;
+    private SeekBar sbBlue;
+    private LinearLayout llPalette;
     private TextView tvRed;
     private TextView tvGreen;
     private TextView tvBlue;
     private TextView tvCode;
+    private EditText etColor;
+    private Button button;
+    private Pattern pattern;
+    private Matcher matcher;
+
+    private static final String HEX_PATTERN = "^[A-Fa-f0-9]{6}$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        red = (SeekBar) findViewById(R.id.seekBarR);
-        green = (SeekBar) findViewById(R.id.seekBarG);
-        blue = (SeekBar) findViewById(R.id.seekBarB);
-        palette = (LinearLayout) findViewById(R.id.palette);
+        sbRed = (SeekBar) findViewById(R.id.seekBarR);
+        sbGreen = (SeekBar) findViewById(R.id.seekBarG);
+        sbBlue = (SeekBar) findViewById(R.id.seekBarB);
+        llPalette = (LinearLayout) findViewById(R.id.palette);
         tvRed = (TextView) findViewById(R.id.textViewR);
         tvGreen = (TextView) findViewById(R.id.textViewG);
         tvBlue = (TextView) findViewById(R.id.textViewB);
         tvCode = (TextView) findViewById(R.id.textViewColorCode);
+        etColor = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
 
-        red.setOnSeekBarChangeListener(this);
-        green.setOnSeekBarChangeListener(this);
-        blue.setOnSeekBarChangeListener(this);
+        sbRed.setOnSeekBarChangeListener(this);
+        sbGreen.setOnSeekBarChangeListener(this);
+        sbBlue.setOnSeekBarChangeListener(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkColor(etColor.getText().toString()))
+                    setColor();
+                else
+                    Toast.makeText(getApplicationContext(), "Enter correct data", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
     private void updateColor() {
         int redColor, greenColor, blueColor, colorCode;
         String colorCodeText;
-        redColor = red.getProgress();
-        greenColor = green.getProgress();
-        blueColor = blue.getProgress();
+        redColor = sbRed.getProgress();
+        greenColor = sbGreen.getProgress();
+        blueColor = sbBlue.getProgress();
 
         colorCode = 0xff000000 + redColor * 0x10000 + greenColor * 0x100
                 + blueColor;
         colorCodeText = Integer.toHexString(colorCode).substring(2);
-        palette.setBackgroundColor(colorCode);
+        llPalette.setBackgroundColor(colorCode);
 
-        tvRed.setText(getResources().getString(R.string.red) + " " + String.valueOf(red.getProgress()));
-        tvGreen.setText(getResources().getString(R.string.green) + " " + String.valueOf(green.getProgress()));
-        tvBlue.setText(getResources().getString(R.string.blue) + " " + String.valueOf(blue.getProgress()));
-        tvCode.setText(getResources().getString(R.string.color_code) + " " + colorCodeText);
+        tvRed.setText(getResources().getString(R.string.red) + " " + String.valueOf(sbRed.getProgress()));
+        tvGreen.setText(getResources().getString(R.string.green) + " " + String.valueOf(sbGreen.getProgress()));
+        tvBlue.setText(getResources().getString(R.string.blue) + " " + String.valueOf(sbBlue.getProgress()));
+        etColor.setText(colorCodeText);
 
+    }
+
+    private void setColor() {
+        String enteredColor = etColor.getText().toString();
+
+        llPalette.setBackgroundColor(Color.parseColor("#" + enteredColor));
+
+        sbRed.setProgress(Integer.parseInt(enteredColor.substring(0, 2), 16));
+        sbGreen.setProgress(Integer.parseInt(enteredColor.substring(2, 4), 16));
+        sbBlue.setProgress(Integer.parseInt(enteredColor.substring(4, 6), 16));
+    }
+
+    private boolean checkColor(final String hex) {
+        pattern = Pattern.compile(HEX_PATTERN);
+        matcher = pattern.matcher(hex);
+
+        return matcher.matches();
     }
 
     @Override
